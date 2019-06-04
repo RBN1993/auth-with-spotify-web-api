db38db55e23c7cf3bddf0cfce51fa97f43267bd6
rbn1993 <surfacerobin@outlook.com>
2019-06-04 22:44:33 +0200

    Implementando client credentials grant , yarn add request, use postman to get playlists, see AuthSpotify in postman

--- api/index.js
+++ api/index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
-
+const request = require('request')
 const { config } = require('./config')
+const encodeBasic = require('./utils/encodeBasic')
 
 const app = express()
 
 // Este middelware permite obtener el json del req.body
 app.use(bodyParser.json())
+
+// Recibe el token y llama al endpoint de spotify para obtener los playlist
+function getUserPlaylists(accessToken, userId) {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null)
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  }
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error)
+      }
+
+      resolve(body)
+    })
+  })
+}
 
 // Vamos a enviar un post, se va a enviar un objeto
 app.post('/api/auth/token', (req, res) => {
@@ -27,6 +51,41 @@ app.get('/api/auth/verify', (req, res, next) => {
     // Con next invocamos el manejador de errores por defecto de express, un html
     next(error)
   }
+})
+
+// Listamos los playlist
+app.get('/api/playlists', async function(req, res, next) {
+  const { userId } = req.query
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${encodeBasic(
+        config.spotifyClientId,
+        config.spotifyClientSecret
+      )}`
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  }
+
+  request.post(authOptions, async function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      next(error)
+    }
+    // No hace falta almacenar el token poque por cada llamada que hacemos
+    // requerimos un nuevo token
+    const accessToken = body.access_token
+    try {
+      const userPlaylists = await getUserPlaylists(accessToken, userId)
+
+      res.json({ playlists: userPlaylists })
+    } catch (error) {
+      next(error)
+    }
+  })
 })
 
 const server = app.listen(5000, () =>
